c3761841596168847bd2c25595ebb595d7bf7030
Gabriel Braz <gabrielbrazcs@gmail.com>
2025-10-05 11:02:10 -0300

    feat(tasks-service): configures postgres database

--- apps/tasks-service/src/app.module.ts
+++ apps/tasks-service/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Postgres } from './config/postgres.config';
 
 @Module({
-	imports: [ConfigModule.forRoot({isGlobal: true, expandVariables: true})],
+	imports: [
+		ConfigModule.forRoot({isGlobal: true, expandVariables: true}),
+		TypeOrmModule.forRootAsync(Postgres.asProvider()),
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })

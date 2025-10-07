a585ee7c8d5880fc54237f4b4462be5c50ad8c9e
Gabriel Braz <gabrielbrazcs@gmail.com>
2025-10-07 13:57:17 -0300

    feat(web): configures themes

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,20 +1,13 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { Navigation } from '@/components'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { ThemeProvider } from '@/components/ThemeProvider'
 
 const RootLayout = () => (
-	<div>
-		<div className="p-2 flex gap-2">
-			<Link to="/" className="[&.active]:font-bold">
-        Home
-			</Link>{' '}
-			<Link to="/login" className="[&.active]:font-bold">
-        Login
-			</Link>
-		</div>
-		<hr />
+	<ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
 		<Outlet />
 		<TanStackRouterDevtools />
-	</div>
+	</ThemeProvider>
 )
 
 export const Route = createRootRoute({ component: RootLayout })

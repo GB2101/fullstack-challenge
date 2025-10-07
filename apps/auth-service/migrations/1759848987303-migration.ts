fe4cfe333434f9fd70b0f61a5d3d01b94039df50
Gabriel Braz <gabrielbrazcs@gmail.com>
2025-10-07 12:28:20 -0300

    feat: configure migrations

--- apps/auth-service/migrations/1759848987303-migration.ts
+++ apps/auth-service/migrations/1759848987303-migration.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Migration1759525610247 implements MigrationInterface {
-    name = 'Migration1759525610247'
+export class Migration1759848987303 implements MigrationInterface {
+    name = 'Migration1759848987303'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "user" ("username" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "creationDate" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "PK_78a916df40e02a9deb1c4b75edb" PRIMARY KEY ("username"))`);
